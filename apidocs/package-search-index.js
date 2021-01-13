3cfc5e8bb6f2249a5794f3bb7b11f1ae0f4203f4
maxwalls <48990854+maxwalls@users.noreply.github.com>
2021-01-13 10:56:57 +0000

    Deploying to gh-pages from  @ 065c74ffc2fe2153abf8f1c9023a21b55b01df75 🚀

---
+++ apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"m":"org.mal_lang.langspec","l":"org.mal_lang.langspec"},{"m":"org.mal_lang.langspec","l":"org.mal_lang.langspec.ttc"}]
